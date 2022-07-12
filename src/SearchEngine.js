5f3aec885dcaae991af86c9a1b35259c8fd3a703
ariola <ariola.markou@gmail.com>
2022-07-12 15:31:41 +0300

    Added on the Search Engine the API Integration

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
 import "./SearchEngine.css";
+import axios from "axios";
 
 export default function SearchEngine() {
   let [keyword, setKeyword] = useState(null);
 
+  function handleResponse(response) {
+    console.log(response.data);
+  }
   function handleSubmit(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
+    let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(url).then(handleResponse);
   }
 
   function updateKeyword(event) {
