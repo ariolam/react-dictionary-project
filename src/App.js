fe65f4ee535f5cb224c099b14b2b1fa3f691556f
ariola <ariola.markou@gmail.com>
2022-07-13 15:19:17 +0300

    Created the footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,33 @@ export default function App() {
     <div className="App">
       <header className="App-header">Dictionary</header>
       <SearchEngine />
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://github.com/ariolam/react-weather-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          Ariola Markou{" "}
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/ariolam/react-weather-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://app.netlify.com/teams/ariolam/overview?_ga=2.31828414.1222677229.1657026152-283144925.1646049587"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+      </footer>
     </div>
   );
 }
